ff3cb523bb940a2f6b67cffb6c85d154b97a46a3
Darwin Lara <lrdarwin75@hotmail.com>
2024-01-07 14:29:24 -0500

    Creacion de controladores y modelos  para las tablas relacionales

---
+++ Controllers/LanguageAudioFilmController.php
@@ -0,0 +1,132 @@
+<?php
+require_once('../../Models/LanguageAudioFilm.php');
+
+//funcion para listar todos los registros 
+function listAudioFilms()
+{
+    $model = new Film(null, null, null, null, null);
+    $filmList = $model->getFilms();
+    return $filmList;
+}
+
+//funcion para leer un registro
+function listFilm($filmId)
+{
+    $model = new Film($filmId, null, null, null, null);
+    $filmObject = $model->getFilm();
+    return $filmObject;
+}
+
+//funcion para guardar el registro
+
+function burnFilm($filmId,$filmTitle, $filmIdplatform,$filmIddirector,$filmPremiereyear,$filmTitleCurrent)
+{
+    $error=[];
+    //Validacion del titulo
+    if (empty($filmTitle)) {
+        $message = 'errortitulovacio';
+        $error[0] = 'error';
+    } else if (!preg_match("/^(.|\s)*\S(.|\s)*$/", $filmTitle)) {
+        $message = 'errortitulovacio';
+        $error[0] = 'error';
+    } else {
+        $error[0] = 'ok';
+    }
+    
+    //Validacion del plataforma
+    if ($filmIdplatform == 0) {
+        $message = 'errorplataformavacio';
+        $error[1] = 'error';
+    } else {
+        $error[1] = 'ok';
+    }
+    
+    //Validacion del director
+    if ($filmIddirector == 0) {
+        $message = 'errordirectorvacio';
+        $error[2] = 'error';
+    } else {
+        $error[2] = 'ok';
+    }
+
+    //Validacion del año
+    if (empty($filmPremiereyear)) {
+        $message = 'erroranovacio';
+        $error[3] = 'error';
+    } else if (!preg_match("/^[0-9]{4}$/", $filmPremiereyear)) {
+        $message = 'erroranoformat';
+        $error[3] = 'error';
+    } else {
+        $error[3] = 'ok';
+    }
+        
+    //No existe error en las entradas
+    if ($error[0] == 'ok' && $error[1] == 'ok' && $error[2] == 'ok' && $error[3] == 'ok')
+    {
+        
+        $model = new Film($filmId,$filmTitle, $filmIdplatform,$filmIddirector,$filmPremiereyear);
+        
+        if ($filmId > 0 )
+        {   
+            //Editar Pelicula
+            if ($filmTitle == $filmTitleCurrent )
+            {
+                $message = 'sametitle'; 
+            }else
+            {
+                //Verificamos si el nombre de la pelicula no existe en la base 
+                $resultFilm = $model->getFilmTitle();
+                if (empty($resultFilm))
+                {
+                    $model = new Film($filmId,$filmTitle, $filmIdplatform,$filmIddirector,$filmPremiereyear);
+                    $result = $model->updateFilm();
+                    if ($result == 1) {
+                        $message = 'edited';
+                    } else 
+                    {
+                        $message = 'errorredited';
+                    }
+                }else
+                {
+                    $message = 'errortitle';
+                }
+            }
+        }else
+        {
+            //Crear Pelicula
+                    
+            //Verificamos si el nombre de la pelicula no existe en la base 
+            $resultFilm = $model->getFilmTitle();
+
+            if (empty($resultFilm))
+            {
+                $result = $model->saveFilm(); 
+                if ($result > 0) {
+                    $message = 'registered';
+                } else {
+                    $message = 'errorregistered';
+                }
+            }else
+            {
+                $message = 'errortitle';
+            }
+        }
+    }  
+     
+    return $message;
+}
+
+//funcion para borrar el estado del registro
+function eraseFilm($filmId)
+{
+    $model = new Film($filmId, null, null,null,null);
+    $result = $model->eliminateFilm();
+    if ($result == 1) {
+        $message = 'erased';
+    } else {
+        $message = 'errorrerased';
+    }           
+    return $message;
+}
+
+?>
---
+++ Models/LanguageAudioFilm.php
@@ -0,0 +1,159 @@
+<?php
+    require_once(__DIR__ . '/../Config/App/Querys.php');
+
+    class LanguageaudioFilm extends Query
+    {
+        //Variables
+        private $id;
+        private $title;
+        private $idplatform;
+        private $iddirector;
+        private $premiereyear;
+        
+        //Constructor
+        public function __construct($idFilm, $titleFilm, $idplatformFilm,$iddirectorFilm,$premiereyearFilm)
+        {
+            $this->id = $idFilm;
+            $this->title = $titleFilm;
+            $this->idplatform = $idplatformFilm;
+            $this->iddirector = $iddirectorFilm;
+            $this->premiereyear = $premiereyearFilm;
+            parent::__construct();
+        }
+        
+        //funcion para leer de base de datos el id 
+        public function getId()
+        {
+            return $this->id;
+        }
+        
+        //funcion para insertar en base de datos el id 
+        public function setId($id)
+        {
+            $this->id = $id;
+        }
+        
+        //funcion para leer de base de datos el titulo 
+        public function getTitle()
+        {
+            return $this->title;
+        }
+        
+        //funcion para insertar en base de datos el titulo
+        public function setTitle($title)
+        {
+            $this->title = $title;
+        }
+
+        //funcion para leer de base de datos la plataforma 
+        public function getIdplatform()
+        {
+            return $this->idplatform;
+        }
+        
+        //funcion para insertar en base de datos la plataforma
+        public function setIdplatform($idplatform)
+        {
+            $this->idplatform = $idplatform;
+        }
+
+        //funcion para leer de base de datos el id director
+        public function getIddirector()
+        {
+            return $this->iddirector;
+        }
+        
+        //funcion para insertar en base de datos el id director
+        public function setIddirector($iddirector)
+        {
+            $this->iddirector = $iddirector;
+        }
+        
+        //funcion para leer de base de datos el año 
+        public function getPremiereyear()
+        {
+            return $this->premiereyear;
+        }
+        
+        //funcion para insertar en base de datos el año
+        public function setPremiereyear($premiereyear)
+        {
+            $this->premiereyear = $premiereyear;
+        }
+
+        //funcion para obtener de base de datos todos los registros 
+        public function listAudioFilms()
+        {
+            $sql = "SELECT * FROM languageaudio_film_detail";
+            $data = $this->selectRecords($sql);
+            $listData = [];
+
+            foreach ($data as $item) {
+                $itemObject = new Film($item['id'], $item['idfilm'], $item['idlanguage']);
+                array_push($listData,$itemObject);
+            }
+            
+            return $listData;
+        }
+        
+        //funcion para leer de base de datos un registro 
+        public function getFilm()
+        {
+            $sql = "SELECT * FROM films WHERE id = ?";
+            $array = array($this->id);
+            $result = $this->selectRecord($sql, $array);
+            return $result;
+        }
+
+        //funcion para leer de base de datos el registro que contiene el nombre
+        public function getFilmTitle()
+        {
+            $sql = "SELECT title FROM films WHERE title = ?";
+            $array = array($this->title);
+            $result = $this->selectRecord($sql, $array);
+            return $result;
+        }
+
+        //funcion para guardar el registro
+        public function saveFilm()
+        {
+            
+            $sql = "INSERT INTO films (title,idplatform,iddirector,premiereyear) VALUES (?,?,?,?)";
+            $array = array($this->title,$this->idplatform,$this->iddirector,$this->premiereyear);
+            $result = $this->insertRecord($sql, $array);
+            return $result;
+        }
+
+        //funcion para actualizar el registro
+        public function updateFilm()
+        {
+            $filmUpdate = false;
+
+            $sql = "UPDATE films SET title=?, idplatform=?, iddirector=?, premiereyear=? WHERE id = ?";
+            $array = array($this->title,$this->idplatform,$this->iddirector,$this->premiereyear,$this->id);
+            $data = $this->saveRecord($sql, $array);
+            
+            if($data){
+                $filmUpdate = true;
+            }
+            return $filmUpdate;
+        }
+
+        //funcion para actualizar el registro
+        public function eliminateFilm()
+        {
+            $filmUpdate = false;
+
+            $sql = "DELETE FROM films WHERE id = ?";
+            $array = array($this->id);
+            $data = $this->saveRecord($sql, $array);
+            
+            if($data){
+                $filmUpdate = true;
+            }
+            return $filmUpdate;
+        }
+        
+    }
+
+ ?>
